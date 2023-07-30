312fb71d89aaba31e454dc48afc8014af30d7351
Fabinsjr <fabintvm@gmail.com>
2023-07-30 17:57:36 +0530

    minor date change

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'NSS UNIVERSITY COLLEGE OF ENGINEERING';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'This is the official website for NSS UCEK.';
